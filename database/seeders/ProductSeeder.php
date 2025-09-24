feca0ea067f7321d04af3f966570be8901ff5f47
Barabr0 <www.marsha698@gmail.com>
2025-09-24 14:15:50 +0700

    ini projek saya

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // DB::table('Product')->insert([
+        //     'name' => 'Basreng',
+        //     'descripsion' => 'basreng enak gurih',
+        //     'price' => 3000,
+        //     'stock' => 5000,
+        // ]);
+
+    }
+}
